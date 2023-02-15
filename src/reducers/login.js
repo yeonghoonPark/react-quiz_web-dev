57d8ff4d997958f9af86d54b646323bd11aea478
yeonghoon Park <huklmnj2@naver.com>
2023-02-15 21:36:09 +0900

    [reducers] login.js / add new action

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -5,6 +5,7 @@ const currentState = {
 
 const ON_LOGIN = "LOGIN/ON_LOGIN";
 const ON_LOGOUT = "LOGIN/ON_LOGOUT";
+const ON_LOGIN_WITH_KAKAO = "LOGIN/ON_LOGIN_WITH_KAKAO";
 
 export const onLogin = () => ({
   type: ON_LOGIN,
@@ -14,12 +15,22 @@ export const onLogout = () => ({
   type: ON_LOGOUT,
 });
 
+export const onLoginWithKakao = () => ({
+  type: ON_LOGIN_WITH_KAKAO,
+});
+
 const login = (state = currentState, action) => {
   switch (action.type) {
     case ON_LOGIN:
       return { ...state, user_id: state.user_id };
     case ON_LOGOUT:
-      return { ...state, user_id: (state.user_id = null) };
+      return {
+        ...state,
+        user_id: (state.user_id = null),
+        user_password: (state.user_password = null),
+      };
+    case ON_LOGIN_WITH_KAKAO:
+      return { ...state, user_id: state.user_id };
     default:
       return state;
   }
