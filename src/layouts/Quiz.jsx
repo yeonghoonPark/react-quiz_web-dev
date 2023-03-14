5cdaed9418588469ff4a59e3424216ac2d218d56
yeonghoon Park <huklmnj2@naver.com>
2023-03-14 15:52:55 +0900

    [layouts] Quiz.jsx / add function & clean up

--- src/layouts/Quiz.jsx
+++ src/layouts/Quiz.jsx
@@ -9,11 +9,11 @@ import { Link } from "react-router-dom";
 import quiz from "../data/quiz.json";
 
 import { useDispatch, useSelector } from "react-redux";
-import {
-  increaseCorrectNumber,
-  resetCorrectNumber,
-  testTimeAttack,
-} from "../reducers/record";
+// import
+// increaseCorrectNumber,
+// resetCorrectNumber,
+// testTimeAttack,
+// "../reducers/record";
 
 const TitleH1 = styled.h1`
   margin-bottom: 1.5rem;
@@ -22,6 +22,7 @@ const TitleH1 = styled.h1`
   font-weight: 700;
   text-align: center;
   pointer-events: none;
+  user-select: none;
   @media all and (max-width: 29.9375rem) {
     padding: 1.5rem 0;
     font-size: 1.5rem;
@@ -53,39 +54,52 @@ const QuizTitleH2 = styled.h2`
   margin-bottom: 1.5rem;
   background-color: var(--color-light-blue);
   pointer-events: none;
+  user-select: none;
+
+  @media all and (max-width: 29.9375rem) {
+    font-size: 1.3rem;
+  }
 `;
 
 const QuizMultipleChoiceP = styled.p`
   width: auto;
   margin: 8px 0;
   font-size: 1.2rem;
+  @media all and (max-width: 29.9375rem) {
+    font-size: 1rem;
+  }
 `;
 
 function Quiz() {
   console.log("[Quiz]");
-  const [quizzes, setQuizzes] = useState([]);
-  const CLASSNAME_HIDDEN = "hidden";
-
-  const correctNumber = useSelector((state) => state.record.correct_number);
+  // const correctNumber = useSelector((state) => state.record.correct_number);
+  let userId = useSelector((state) => state.login.user_id);
   const dispatch = useDispatch();
+
+  const [quizzes, setQuizzes] = useState([]);
 
   const [minute, setMinute] = useState("00");
   const [second, setSecond] = useState("00");
   const [millisecond, setMillisecond] = useState("00");
+
+  const [isReady, setIsReady] = useState(false);
+  const [isCountdown, setIsCountdown] = useState(false);
+  const [isStart, setIsStart] = useState(false);
+
+  let [correctNumber, setCorrectNumber] = useState(0);
+  let [count, setCount] = useState(3);
+
+  const [timerInterval, setTimerInterval] = useState();
+  const countIntervalRef = useRef(null);
+  const CLASSNAME_HIDDEN = "hidden";
 
   const mixArrayRandomly = (array) => {
     console.log("[mixArrayRandomly]");
     return array.sort(() => Math.random() - 0.5);
   };
 
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    return () => dispatch(resetCorrectNumber());
-  }, []);
-
   const createHTMLString = () => {
-    console.log("[createHTMLString]");
+    // console.log("[createHTMLString]");
     const result = [];
     for (let index = 0; index < 10; index++) {
       if (index === 0) {
@@ -98,6 +112,7 @@ function Quiz() {
               <BaseSpan
                 className='multiple-choice'
                 pointer
+                userSelectNone
                 onClick={(e) => {
                   countCorrect(
                     quizzes[index]?.multiple_choice_view1,
@@ -113,6 +128,7 @@ function Quiz() {
               <BaseSpan
                 className='multiple-choice'
                 pointer
+                userSelectNone
                 onClick={(e) => {
                   countCorrect(
                     quizzes[index]?.multiple_choice_view2,
@@ -128,6 +144,7 @@ function Quiz() {
               <BaseSpan
                 className='multiple-choice'
                 pointer
+                userSelectNone
                 onClick={(e) => {
                   countCorrect(
                     quizzes[index]?.multiple_choice_view3,
@@ -151,6 +168,7 @@ function Quiz() {
               <BaseSpan
                 className='multiple-choice'
                 pointer
+                userSelectNone
                 onClick={(e) => {
                   countCorrect(
                     quizzes[index]?.multiple_choice_view1,
@@ -166,6 +184,7 @@ function Quiz() {
               <BaseSpan
                 className='multiple-choice'
                 pointer
+                userSelectNone
                 onClick={(e) => {
                   countCorrect(
                     quizzes[index]?.multiple_choice_view2,
@@ -181,6 +200,7 @@ function Quiz() {
               <BaseSpan
                 className='multiple-choice'
                 pointer
+                userSelectNone
                 onClick={(e) => {
                   countCorrect(
                     quizzes[index]?.multiple_choice_view3,
@@ -199,22 +219,14 @@ function Quiz() {
     return result;
   };
 
-  // const setDisplayItems = () => {
-  //   console.log("[setDisplayItems]");
-  //   const quizBoxes = document.querySelectorAll(".quiz-box");
-
-  //   quizBoxes.forEach((quizBox, index) =>
-  //     index !== 0 ? quizBox.classList.add(CLASSNAME_HIDDEN) : null,
-  //   );
-  // };
-
   const removeElement = (e) => {
     // console.log("[removeElement]");
     const currentQuizBox = e.target.parentNode.parentNode;
     const nextQuizBox = currentQuizBox.nextSibling;
 
-    nextQuizBox ? nextQuizBox.classList.remove(CLASSNAME_HIDDEN) : stopTimer();
-
+    nextQuizBox
+      ? nextQuizBox.classList.remove(CLASSNAME_HIDDEN)
+      : finishedQuiz();
     currentQuizBox.remove();
   };
 
@@ -222,15 +234,14 @@ function Quiz() {
     console.log("[countCorrect]");
     if (clickedItem === correct) {
       console.log("정답");
-      dispatch(increaseCorrectNumber());
+      // dispatch(increaseCorrectNumber());
+      setCorrectNumber((correctNumber += 1));
     } else {
       console.log("오답");
     }
   };
 
   const addZero = (number) => (number < 10 ? "0" + number : "" + number);
-
-  const [timerInterval, setTimerInterval] = useState();
 
   const startTimer = () => {
     console.log("[startTimer]");
@@ -250,35 +261,73 @@ function Quiz() {
   const stopTimer = () => {
     console.log("[stopTimer]");
     clearInterval(timerInterval);
+    setIsStart(true);
   };
 
   const onHandleStart = () => {
-    setIsReady(true);
-    setQuizzes(mixArrayRandomly(quiz.data));
-    startTimer();
+    console.log("[onHandleStart]");
+    setIsCountdown(true);
+    countIntervalRef.current = setInterval(() => {
+      setCount((count -= 1));
+      if (count === 0) {
+        setCount("Go!!");
+        clearInterval(countIntervalRef.current);
+        countIntervalRef.current = null;
+      }
+    }, 1000);
+    setTimeout(() => {
+      setIsReady(true);
+      setQuizzes(mixArrayRandomly(quiz.data));
+      startTimer();
+    }, 4000);
   };
+
+  const finishedQuiz = () => {
+    console.log("[finishedQuiz]");
+    setIsStart(true);
+    stopTimer();
+  };
+
+  const returnText = (correctNumber) => {
+    // 0 ~ 1
+    if (correctNumber < 2) {
+      return `...위로의 말을 전합니다.`;
+      // 2 ~ 5
+    } else if (correctNumber > 1 && correctNumber < 6) {
+      return `조금 더 분발해주세요.`;
+      // 6 ~ 7
+    } else if (correctNumber > 5 && correctNumber < 8) {
+      return `상식이 뛰어나십니다.`;
+      // 8 ~ 9
+    } else if (correctNumber > 7 && correctNumber < 10) {
+      return `훌륭해요!!`;
+      // 10
+    } else {
+      return `축하합니다!!`;
+    }
+  };
+
+  const onHandleRestart = () => {
+    // dispatch(resetCorrectNumber());
+    setCorrectNumber(0);
+    setCount(3);
+    setIsReady(false);
+    setIsCountdown(false);
+    setIsStart(false);
+  };
+
+  useEffect(() => {
+    return () => {
+      // dispatch(resetCorrectNumber());
+
+      clearInterval(countIntervalRef.current);
+      countIntervalRef.current = null;
+      onHandleRestart();
+    };
+  }, []);
 
   return (
     <BaseContainer>
-      <button
-        onClick={() => {
-          setIsReady(true);
-          setQuizzes(mixArrayRandomly(quiz.data));
-          // setDisplayItems();
-          startTimer();
-        }}
-      >
-        Start
-      </button>
-      <button
-        onClick={() => {
-          stopTimer();
-        }}
-      >
-        Stop
-      </button>
-      <button onClick={() => {}}>Incease</button>
-      <span>{}</span>
       <TitleH1>Quiz</TitleH1>
       {!isReady ? (
         <QuizContainer>
@@ -288,50 +337,94 @@ function Quiz() {
             alignItems={"center"}
             padding={"3rem 0"}
           >
-            <BaseSpan margin={"0 0 3rem"} fontSize={"2rem"}>
-              준비되셨나요?
-            </BaseSpan>
-            <BaseButton className={"large"} onClick={onHandleStart}>
-              Start
-            </BaseButton>
+            {!isCountdown ? (
+              <>
+                <BaseSpan userSelectNone margin={"0 0 3rem"} fontSize={"2rem"}>
+                  준비되셨나요?
+                </BaseSpan>
+                <BaseButton className={"large"} onClick={onHandleStart}>
+                  Start
+                </BaseButton>
+              </>
+            ) : (
+              <BaseSpan userSelectNone margin={"1.8rem 0"} fontSize={"4rem"}>
+                {count}
+              </BaseSpan>
+            )}
           </BaseDiv>
         </QuizContainer>
       ) : (
         <QuizContainer>
-          <BaseDiv
-            display={"flex"}
-            justifyContent={"space-between"}
-            alignItems={"center"}
-            padding={"0 0 1.5rem"}
-            mobileFlexDirection={"column"}
-            mobileAlignItems={"flex-start"}
-          >
-            <BaseSpan
-              className='tablet-margin-bottom'
-              padding={"8px"}
-              border={"1px solid var(--color-gray-500)"}
-              borderRadius={"var(--radius-standard)"}
-              pointerEventsNone
+          {!isStart ? (
+            <>
+              <BaseDiv
+                display={"flex"}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+                padding={"0 0 1.5rem"}
+                mobileFlexDirection={"column"}
+                mobileAlignItems={"flex-start"}
+              >
+                <BaseSpan
+                  className='tablet-margin-bottom mobile-font-s'
+                  padding={"8px"}
+                  border={"1px solid var(--color-gray-500)"}
+                  borderRadius={"var(--radius-standard)"}
+                  userSelectNone
+                >
+                  <FaCheckCircleIcon /> 맞춘 갯수:{" "}
+                  <BaseSpan className={"danger"} userSelectNone>
+                    {correctNumber}
+                  </BaseSpan>
+                </BaseSpan>
+                <BaseSpan
+                  className='mobile-font-s'
+                  padding={"8px"}
+                  border={"1px solid var(--color-gray-500)"}
+                  borderRadius={"var(--radius-standard)"}
+                  userSelectNone
+                >
+                  <FaClockIcon /> 소요 시간: {minute}:{second}.
+                  <BaseSpan className={"danger"} userSelectNone>
+                    {millisecond}
+                  </BaseSpan>
+                </BaseSpan>
+              </BaseDiv>
+              <div>{createHTMLString()}</div>
+            </>
+          ) : (
+            <BaseDiv
+              display={"flex"}
+              flexDirection={"column"}
+              alignItems={"center"}
+              padding={"3rem 0"}
             >
-              <FaCheckCircleIcon /> 맞춘 갯수:{" "}
-              <BaseSpan className={"danger"} pointerEventsNone>
-                {correctNumber}
+              <QuizTitleH2 className='bg-dark-blue'>'{userId}'</QuizTitleH2>
+              <BaseSpan margin={"0 0 1.5rem"}>
+                {returnText(correctNumber)}
               </BaseSpan>
-            </BaseSpan>
-            <BaseSpan
-              padding={"8px"}
-              border={"1px solid var(--color-gray-500)"}
-              borderRadius={"var(--radius-standard)"}
-              pointerEventsNone
-            >
-              <FaClockIcon /> 소요 시간: {minute}:{second}.
-              <BaseSpan className={"danger"} pointerEventsNone>
-                {millisecond}
-              </BaseSpan>
-            </BaseSpan>
-          </BaseDiv>
+              <BaseDiv
+                display={"flex"}
+                flexDirection={"column"}
+                margin={"0 0 1.5rem"}
+                padding={"0"}
+              >
+                <BaseSpan margin={"8px"}>
+                  맞춘 갯수 :{" "}
+                  <BaseSpan className={"danger"}>{correctNumber}</BaseSpan> 문제
+                </BaseSpan>
+                <BaseSpan margin={"8px"}>
+                  소요 시간 : {minute}:{second}:
+                  <BaseSpan className={"danger"}>{millisecond}</BaseSpan>
+                </BaseSpan>
+                <BaseSpan margin={"8px"}>등수 : 없음</BaseSpan>
+              </BaseDiv>
 
-          {createHTMLString()}
+              <BaseButton className={"large"} onClick={onHandleRestart}>
+                재도전
+              </BaseButton>
+            </BaseDiv>
+          )}
         </QuizContainer>
       )}
     </BaseContainer>
