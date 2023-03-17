15dec2607aa14cb834f8891367577fd53a78669c
yeonghoon Park <huklmnj2@naver.com>
2023-03-17 21:18:57 +0900

    [components] BaseContainer.jsx / edit file name
    - AppContainer.jsx

--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
+  position: relative;
+  z-index: 1;
   max-width: 1200px;
   height: calc(100vh - 66px);
   margin: 66px auto 0;
   padding: 1.5rem;
 `;
 
-function BaseContainer({ children }) {
+function AppContainer({ children }) {
   return <StyledContainer>{children}</StyledContainer>;
 }
 
-export default BaseContainer;
+export default AppContainer;
