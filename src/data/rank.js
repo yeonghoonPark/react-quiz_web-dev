1d18bbe4b900ad5ad101d3996b5d06cd73f1cff2
yeonghoon Park <huklmnj2@naver.com>
2023-03-17 21:19:37 +0900

    [data] quiz.json, rank.js
    - quiz.json => quiz.js
    - rank.js (create)

---
+++ src/data/rank.js
@@ -0,0 +1,74 @@
+const rank = [
+  {
+    user_id: "간디",
+    correct_number: "8",
+    minute: "01",
+    second: "08",
+    millisecond: "99",
+  },
+  {
+    user_id: "이영자",
+    correct_number: "7",
+    minute: "01",
+    second: "11",
+    millisecond: "15",
+  },
+  {
+    user_id: "석가모니",
+    correct_number: "7",
+    minute: "01",
+    second: "05",
+    millisecond: "93",
+  },
+  {
+    user_id: "Mike",
+    correct_number: "6",
+    minute: "01",
+    second: "11",
+    millisecond: "43",
+  },
+  {
+    user_id: "Jamse Tylor",
+    correct_number: "5",
+    minute: "01",
+    second: "15",
+    millisecond: "34",
+  },
+  {
+    user_id: "운영자",
+    correct_number: "3",
+    minute: "02",
+    second: "08",
+    millisecond: "56",
+  },
+  {
+    user_id: "나그네",
+    correct_number: "5",
+    minute: "01",
+    second: "19",
+    millisecond: "73",
+  },
+  {
+    user_id: "세종대왕",
+    correct_number: "6",
+    minute: "01",
+    second: "18",
+    millisecond: "24",
+  },
+  {
+    user_id: "퀴즈왕",
+    correct_number: "6",
+    minute: "01",
+    second: "08",
+    millisecond: "08",
+  },
+  {
+    user_id: "박지성",
+    correct_number: "5",
+    minute: "01",
+    second: "56",
+    millisecond: "88",
+  },
+];
+
+export default rank;
