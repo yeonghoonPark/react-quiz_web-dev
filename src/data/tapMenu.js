7dafc43813d9aae5910f349fb076825fdd8e7556
yeonghoon Park <huklmnj2@naver.com>
2023-03-21 19:09:09 +0900

    [data] tapMenu.js / create file

---
+++ src/data/tapMenu.js
@@ -0,0 +1,36 @@
+const tapMenu = [
+  {
+    uniq_no: 1,
+    html_for: "total",
+    value: "total",
+    id: "total",
+    name: "radio",
+    message: "전체",
+  },
+  {
+    uniq_no: 2,
+    html_for: "announcement",
+    value: "announcement",
+    id: "announcement",
+    name: "radio",
+    message: "공지",
+  },
+  {
+    uniq_no: 3,
+    html_for: "request",
+    value: "request",
+    id: "request",
+    name: "radio",
+    message: "요청",
+  },
+  {
+    uniq_no: 4,
+    html_for: "chitchat",
+    value: "chitchat",
+    id: "chitchat",
+    name: "radio",
+    message: "잡담",
+  },
+];
+
+export default tapMenu;
