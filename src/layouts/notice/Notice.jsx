91e580d4451f8900adf4611a64ecf69a5b56213d
yeonghoon Park <huklmnj2@naver.com>
2023-03-21 19:10:23 +0900

    [layouts] Notice.jsx / create HTML

--- src/layouts/notice/Notice.jsx
+++ src/layouts/notice/Notice.jsx
@@ -1,14 +1,56 @@
+import styled from "styled-components";
+import AppContainer from "../../components/AppContainer";
+import AppTitle from "../../components/AppTitle";
+import BaseInputRadio from "../../components/base/BaseInputRadio";
 import BaseButton from "../../components/base/BaseButton";
 import { Link } from "react-router-dom";
+import { useCallback, useRef, useState } from "react";
+import tapMenu from "../../data/tapMenu";
+
+const RadioGroupContainer = styled.div`
+  position: relative;
+  display: flex;
+  width: 70%;
+  min-width: 314px;
+  margin: 0 auto;
+  border: 2px solid var(--color-gray-500);
+  border-radius: var(--radius-standard);
+`;
 
 function Notice() {
+  console.log("[Notice]");
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onHandleTapMenu = useCallback(
+    (i) => {
+      setCurrentIndex(i);
+    },
+    [currentIndex],
+  );
+
   return (
-    <div style={{ marginTop: "66px" }}>
-      <h1>Notice Test</h1>
-      <Link to={"/"}>
-        <BaseButton message={"Home"} />
-      </Link>
-    </div>
+    <AppContainer>
+      <AppTitle>Notice</AppTitle>
+
+      <RadioGroupContainer>
+        {tapMenu.map((cV, i) => {
+          return (
+            <BaseInputRadio
+              key={cV.uniq_no}
+              htmlFor={cV.html_for}
+              id={cV.id}
+              value={cV.value}
+              name={cV.name}
+              onClick={() => onHandleTapMenu(i)}
+              className={currentIndex === i ? "checked" : "checked-none"}
+            >
+              {cV.message}
+            </BaseInputRadio>
+          );
+        })}
+      </RadioGroupContainer>
+    </AppContainer>
   );
 }
 
